344616598246f3baa62ae2cb907f2543d1f78213
redosansms <thomasgalican@gmail.com>
2025-03-18 23:35:15 +0100

    Add files via upload

---
+++ convertisseur degrée c° fahrenheit.py
@@ -0,0 +1,10 @@
+print("hello, world démarage système")
+reponse = int(input("comment allez vous sur une échele de 1 a 10" ))
+print(type(reponse))
+
+print("parfait commencons la convertion")
+celsius = float(input("entrer la temperature en celsius)"))
+fahrenheit = celsius * 9/5 + 32
+print(fahrenheit)
+print("la temperature en fahraneit est de ")
+print(fahrenheit)
